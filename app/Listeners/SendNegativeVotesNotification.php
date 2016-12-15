6be844d09aae13bce11e3b3f6fc7ced8d8770c75
Joren Van Hee <jorenvanhee@gmail.com>
2016-12-16 00:46:36 +0100

    Counting of consecutive negative counts works

--- app/Listeners/SendNegativeVotesNotification.php
+++ app/Listeners/SendNegativeVotesNotification.php
@@ -3,8 +3,10 @@
 namespace App\Listeners;
 
 use App\Events\VoteCasted;
-use Illuminate\Queue\InteractsWithQueue;
+use App\Models\VoteOption;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Cache;
 
 class SendNegativeVotesNotification
 {
@@ -26,6 +28,36 @@ class SendNegativeVotesNotification
      */
     public function handle(VoteCasted $event)
     {
-        //
+        if (! $event->vote->vote->equals(new VoteOption(VoteOption::SAD))) {
+            return $this->resetConsecutiveVotesCounter();
+        }
+
+        $this->incrementConsecutiveVotesCounter();
+
+        if ($this->hasReachedConsecutiveVotesLimit()) {
+            $this->resetConsecutiveVotesCounter();
+
+            // Do stuff
+        }
+    }
+
+    protected function hasReachedConsecutiveVotesLimit()
+    {
+        return Cache::get($this->getCounterKey()) == 3;
+    }
+
+    protected function resetConsecutiveVotesCounter()
+    {
+        Cache::forever($this->getCounterKey(), 0);
+    }
+
+    protected function incrementConsecutiveVotesCounter()
+    {
+        Cache::increment($this->getCounterKey());
+    }
+
+    protected function getCounterKey()
+    {
+        return 'ConsecutiveNegativeVotes';
     }
 }
