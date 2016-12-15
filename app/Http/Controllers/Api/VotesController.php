8dd863901f28d7db305e572b6e31bc60b33e90a4
Joren Van Hee <jorenvanhee@gmail.com>
2016-12-15 23:04:40 +0100

    Add api/vote create endpoint

---
+++ app/Http/Controllers/Api/VotesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Vote;
+use App\Models\VoteOption;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Validation\Rule;
+
+class VotesController extends Controller
+{
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'vote' => ['required', Rule::in(VoteOption::values())],
+        ]);
+
+        $vote = Vote::create($request->only('vote'));
+
+        return response($vote, Response::HTTP_CREATED);
+    }
+}
--- app/Models/VoteOption.php
+++ app/Models/VoteOption.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use MyCLabs\Enum\Enum;
+use JsonSerializable;
 
-class VoteOption extends Enum
+class VoteOption extends Enum implements JsonSerializable
 {
     const SAD = 'sad';
     const NEUTRAL = 'neutral';
     const HAPPY = 'happy';
+
+    public function jsonSerialize()
+    {
+        return $this->value;
+    }
 }
